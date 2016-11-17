7b5d66f28bad97239d5b49b5f1b60a28b3576a2d
Brendan McGrath <redmcg@redmandi.dyndns.org>
2016-11-17 21:00:20 +1100

    Change name from 'focaltech' to 'asus-fte'

--- src/hid-asus-fte.c
+++ src/hid-asus-fte.c
@@ -18,7 +18,7 @@
 
 MODULE_AUTHOR("Brendan McGrath <redmcg@redmandi.dyndns.org>");
 MODULE_AUTHOR("Victor Vlasenko <victor.vlasenko@sysgears.com>");
-MODULE_DESCRIPTION("FocalTech TouchPad");
+MODULE_DESCRIPTION("ASUS FTE I2C HID TouchPad");
 MODULE_LICENSE("GPL");
 
 
@@ -40,19 +40,18 @@ MODULE_LICENSE("GPL");
 #define CONTACT_SIZE 5
 
 /**
- * struct focaltech_t - Tracks FocalTech TouchPad data.
+ * struct asus_t - Tracks ASUS FTE I2C HID TouchPad data.
  * @input: Input device through which we report events.
  */
-struct focaltech_t {
-		struct input_dev *input;
+struct asus_t {
+	struct input_dev *input;
 };
 
-static int focaltech_raw_event(struct hid_device *hdev,
-				struct hid_report *report, u8 *data, int size)
+static int asus_raw_event(struct hid_device *hdev, struct hid_report *report, u8 *data, int size)
 {
 	int i, contactNum = 0;
-	struct focaltech_t *ft = hid_get_drvdata(hdev);
-	struct input_dev *input = ft->input;
+	struct asus_t *drvdata = hid_get_drvdata(hdev);
+	struct input_dev *input = drvdata->input;
 
 	for (i = 0; i < MAX_CONTACTS; i++) {
 		int down = data[1] & (0x08 << i);
@@ -64,7 +63,6 @@ static int focaltech_raw_event(struct hid_device *hdev,
 
 		input_mt_slot(input, i);
 		input_mt_report_slot_state(input, toolType, down);
-
 
 		if (down) {
 			input_report_abs(input, ABS_MT_POSITION_X, x);
@@ -82,17 +80,17 @@ static int focaltech_raw_event(struct hid_device *hdev,
 	return 1;
 }
 
-static int focaltech_setup_input(struct hid_device *hdev, struct input_dev *input)
+static int asus_setup_input(struct hid_device *hdev, struct input_dev *input)
 {
 	int ret = input_mt_init_slots(input, MAX_CONTACTS, 0);
-	struct focaltech_t *ft = hid_get_drvdata(hdev);
+	struct asus_t *drvdata = hid_get_drvdata(hdev);
 
 	if (ret) {
-		hid_err(hdev, "focaltech input mt init slots failed: %d\n", ret);
+		hid_err(hdev, "ASUS FTE input mt init slots failed: %d\n", ret);
 		return ret;
 	}
 
-	ft->input = input;
+	drvdata->input = input;
 
 	__set_bit(EV_KEY, input->evbit);
 	__set_bit(BTN_LEFT, input->keybit);
@@ -127,21 +125,20 @@ static int focaltech_setup_input(struct hid_device *hdev, struct input_dev *inpu
 	return 0;
 }
 
-static int focaltech_input_configured(struct hid_device *hdev,
-				struct hid_input *hi)
+static int asus_input_configured(struct hid_device *hdev, struct hid_input *hi)
 {
-	int ret = focaltech_setup_input(hdev, hi->input);
+	int ret = asus_setup_input(hdev, hi->input);
 	if (ret)
 		return ret;
 
 	return 0;
 }
 
-static int focaltech_input_mapping(struct hid_device *hdev,
+static int asus_input_mapping(struct hid_device *hdev,
 				struct hid_input *hi, struct hid_field *field,
 				struct hid_usage *usage, unsigned long **bit, int *max)
 {
-	/* Don't map anything from the HID report. We do it all manually in focaltech_setup_input */
+	/* Don't map anything from the HID report. We do it all manually in asus_setup_input */
 	return -1;
 }
 
@@ -157,42 +154,41 @@ static int start_multitouch(struct hid_device *hdev) {
 }
 
 #ifdef CONFIG_PM
-static int focaltech_resume(struct hid_device *hdev) {
+static int asus_resume(struct hid_device *hdev) {
 	return start_multitouch(hdev);
 }
 #endif
 
-static int focaltech_probe(struct hid_device *hdev,
-		const struct hid_device_id *id)
+static int asus_probe(struct hid_device *hdev, const struct hid_device_id *id)
 {
 	int ret;
 	struct hid_report *report;
-	struct focaltech_t *ft;
+	struct asus_t *drvdata;
 
-	ft = devm_kzalloc(&hdev->dev, sizeof(*ft), GFP_KERNEL);
-	if (ft == NULL) {
-		hid_err(hdev, "can't alloc focaltech descriptor\n");
+	drvdata = devm_kzalloc(&hdev->dev, sizeof(*drvdata), GFP_KERNEL);
+	if (drvdata == NULL) {
+		hid_err(hdev, "Can't alloc ASUS FTE descriptor\n");
 		return -ENOMEM;
 	}
 
-	hid_set_drvdata(hdev, ft);
+	hid_set_drvdata(hdev, drvdata);
 
 	hdev->quirks = HID_QUIRK_NO_INIT_REPORTS;
 
 	ret = hid_parse(hdev);
 	if (ret) {
-		hid_err(hdev, "focaltech hid parse failed: %d\n", ret);
+		hid_err(hdev, "ASUS FTE hid parse failed: %d\n", ret);
 		return ret;
 	}
 
 	ret = hid_hw_start(hdev, HID_CONNECT_DEFAULT);
 	if (ret) {
-		hid_err(hdev, "focaltech hw start failed: %d\n", ret);
+		hid_err(hdev, "ASUS FTE hw start failed: %d\n", ret);
 		return ret;
 	}
 
-	if (!ft->input) {
-		hid_err(hdev, "focaltech input not registered\n");
+	if (!drvdata->input) {
+		hid_err(hdev, "ASUS FTE input not registered\n");
 		ret = -ENOMEM;
 		goto err_stop_hw;
 	}
@@ -200,7 +196,7 @@ static int focaltech_probe(struct hid_device *hdev,
 	report = hid_register_report(hdev, HID_INPUT_REPORT, INPUT_REPORT_ID);
 
 	if (!report) {
-		hid_err(hdev, "unable to register input report\n");
+		hid_err(hdev, "Unable to register input report\n");
 		ret = -ENOMEM;
 		goto err_stop_hw;
 	}
@@ -215,24 +211,24 @@ err_stop_hw:
 	return ret;
 }
 
-static const struct hid_device_id focaltech_touchpad[] = {
+static const struct hid_device_id asus_touchpad[] = {
 	{ HID_I2C_DEVICE(VENDOR_ID,
 		DEVICE_ID), .driver_data = 0 },
 	{ }
 };
-MODULE_DEVICE_TABLE(hid, focaltech_touchpad);
+MODULE_DEVICE_TABLE(hid, asus_touchpad);
 
-static struct hid_driver focaltech_driver = {
-	.name = "hid-focaltech",
-	.id_table = focaltech_touchpad,
-	.probe = focaltech_probe,
-	.input_mapping = focaltech_input_mapping,
-	.input_configured = focaltech_input_configured,
+static struct hid_driver asus_driver = {
+	.name = "hid-asus-fte",
+	.id_table = asus_touchpad,
+	.probe = asus_probe,
+	.input_mapping = asus_input_mapping,
+	.input_configured = asus_input_configured,
 #ifdef CONFIG_PM
-	.resume			= focaltech_resume,
-	.reset_resume		= focaltech_resume,
+	.resume			= asus_resume,
+	.reset_resume		= asus_resume,
 #endif
-	.raw_event = focaltech_raw_event
+	.raw_event = asus_raw_event
 };
 
-module_hid_driver(focaltech_driver);
+module_hid_driver(asus_driver);
