42d234e7150364d3f2ad82d67dc7affe51a82691
Victor Vlasenko <victor.vlasenko@sysgears.com>
2016-11-23 08:58:22 +0200

    Ignore only error output from dkms remove

--- dkms-add.sh
+++ dkms-add.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-sudo dkms remove asus-fte/1.0 --all
-sudo dkms remove asus/1.0 --all
+sudo dkms remove asus-fte/1.0 --all 2>/dev/null
+sudo dkms remove asus/1.0 --all 2>/dev/null
 sudo dkms add ../hid-asus-dkms
 sudo dkms install -m asus -v 1.0
 ./dev-attach.sh
--- dkms-remove.sh
+++ dkms-remove.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-sudo dkms remove asus-fte/1.0 --all
-sudo dkms remove asus/1.0 --all
+sudo dkms remove asus-fte/1.0 --all 2>/dev/null
+sudo dkms remove asus/1.0 --all 2>/dev/null
 ./dev-restore.sh
