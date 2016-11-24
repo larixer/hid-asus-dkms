cca4cd32fa57678bff4f510b3672d04f55259402
Brendan McGrath <redmcg@redmandi.dyndns.org>
2016-11-24 17:22:21 +1100

    Don't use a dir name when performing dkms add

--- dkms-add.sh
+++ dkms-add.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 sudo dkms remove asus-fte/1.0 --all 2>/dev/null
 sudo dkms remove asus/1.0 --all 2>/dev/null
-sudo dkms add ../hid-asus-dkms
+sudo dkms add .
 sudo dkms install -m asus -v 1.0
 ./dev-attach.sh
