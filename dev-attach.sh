80ab80437a0addfa437f0c5676fb51a4f400975b
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2017-02-08 09:45:21 +0100

    dev-attach.sh: Run `dmesg` with sudo
    In my system (Debian stretch), a user does not have permissions to
    read `dmesg` output, and this command results in:
    
    > dmesg: read kernel buffer failed: Operation not permitted
    
    and subsequent fail.
    
    Add sudo before the dmesg call to fix it.

--- dev-attach.sh
+++ dev-attach.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-DEV_NO=`dmesg | grep "on i2c-FTE100" | sed -n 's/[^a-z]*[^0-9]*\([0-9A-F\.:]*\):.*/\1/p' | tail -1`
+DEV_NO=`sudo dmesg | grep "on i2c-FTE100" | sed -n 's/[^a-z]*[^0-9]*\([0-9A-F\.:]*\):.*/\1/p' | tail -1`
 sudo modprobe hid_asus
 if [ ! -h /sys/bus/hid/drivers/hid-asus/$DEV_NO ]; then
 	echo Rebinding $DEV_NO to hid-asus
